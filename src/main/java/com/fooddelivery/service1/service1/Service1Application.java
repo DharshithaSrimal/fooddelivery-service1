0a1e2107f9ff0a750974069a1c80a04ab267764e
Dharshitha Senevirathne <dharshithasrimal@gmail.com>
2023-02-04 10:39:54 +0530

    Add files via upload

--- src/main/java/com/fooddelivery/service1/service1/Service1Application.java
+++ src/main/java/com/fooddelivery/service1/service1/Service1Application.java
@@ -2,10 +2,32 @@ package com.fooddelivery.service1.service1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fooddelivery.service1.domain.Order;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/orders")
 public class Service1Application {
 
+    private OrderDao orderDao;
+
+	public Service1Application(OrderDao orderDao){
+		this.orderDao = orderDao;
+	}
+
+    @GetMapping
+    public List<Order> fetchOrders() {
+        return orderDao.getOrders().stream().
+                sorted(Comparator.comparing(Order::getPrice)).collect(Collectors.toList());
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(Service1Application.class, args);
 	}
